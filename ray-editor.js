400f57f2e7bfcdc1753fa4791a8c125ce3369ac0
Rohan Yeole <rayeole@gmail.com>
2025-05-08 17:33:30 +0530

    fixed issue

--- ray-editor.js
+++ ray-editor.js
@@ -98,7 +98,7 @@ export function initEditor(containerId, config = {}) {
       imageUpload: {
          keyname: "imageUpload",
          label: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-image-icon lucide-image"><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/><circle cx="9" cy="9" r="2"/><path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21"/></svg>`,
-         action: () => triggerImageUpload(config)
+         action: () => triggerImageUpload(config, container)
       },
       fileUpload: {
          keyname: "file",
@@ -616,7 +616,7 @@ function replacePlaceholderWithFileLink(placeholder, filename, url) {
    placeholder.replaceWith(link);
 }
 
-function triggerImageUpload(config) {
+function triggerImageUpload(config, container) {
    const input = document.createElement('input');
    input.type = 'file';
    input.accept = 'image/*';
